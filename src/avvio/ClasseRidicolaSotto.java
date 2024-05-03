1fd46b6c077b1c0c8c9ad37ec89527abe3322204
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-03 11:46:33 +0200

    versione2

--- src/avvio/ClasseRidicolaSotto.java
+++ src/avvio/ClasseRidicolaSotto.java
@@ -5,5 +5,6 @@ public class ClasseRidicolaSotto
     public static void main(String[] args) 
     {
         System.out.println("sono una classe molto semplice");    
+        System.out.println("CIAO SONO SEMPRE IO");
     }
 }
