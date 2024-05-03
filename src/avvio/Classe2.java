d0e846ead80f7427406e7a575cdd349625d59d73
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-03 12:06:33 +0200

    v5

--- src/avvio/Classe2.java
+++ src/avvio/Classe2.java
@@ -2,6 +2,7 @@ package avvio;
 
 public class Classe2 
 {
+    static int ciao = 2;
     public static void main(String[] args) 
     {
         System.out.println("Questa è molto importante evidentemente");
