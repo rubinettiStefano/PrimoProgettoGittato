ff048d8eb9f157eb70cfd33deca72f22615f3281
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-03 11:45:23 +0200

    versione1

---
+++ src/avvio/ClasseRidicolaSotto.java
@@ -0,0 +1,9 @@
+package avvio;
+
+public class ClasseRidicolaSotto 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("sono una classe molto semplice");    
+    }
+}
